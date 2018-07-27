46d88ba632bb5a4cedcde3fdc0ceedd6b89f9712
foolchen <chenchong8806@gmail.com>
2018-07-27 10:24:19 +0800

    警用了Glide的清单解析

--- app/src/main/java/com/foolchen/arch/samples/image/ArchGlideModule.kt
+++ app/src/main/java/com/foolchen/arch/samples/image/ArchGlideModule.kt
@@ -4,4 +4,10 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
 @GlideModule
-class ArchGlideModule : AppGlideModule()
+class ArchGlideModule : AppGlideModule() {
+
+  // 禁用清单解析。这样可以改善 Glide 的初始启动时间，并避免尝试解析元数据时的一些潜在问题
+  override fun isManifestParsingEnabled(): Boolean {
+    return false
+  }
+}
